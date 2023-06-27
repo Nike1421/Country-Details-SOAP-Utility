47d67c6e0513a8bf6c6ce3cfd2d9556dcc0d09db
Nike1421 <ommiemo54@gmail.com>
2023-06-27 22:24:03 +0530

    Add Input Panel Widgets

--- src/nike1421/countryinfo/ui/CountryInfoUtilityUI.java
+++ src/nike1421/countryinfo/ui/CountryInfoUtilityUI.java
@@ -1,5 +1,7 @@
 package nike1421.countryinfo.ui;
 
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -9,10 +11,12 @@ import javax.swing.WindowConstants;
 
 public class CountryInfoUtilityUI {
 	JFrame mainUIFrame;
-	JLabel inputLabel;
+	JLabel titleLabel;
 	JPanel componentPanel;
+	
 	JButton retrieveResultsButton;
-	JComboBox countinentComboBox;
+	JLabel inputLabel;
+	JComboBox continentComboBox;
 	JComboBox countryComboBox;
 	
 	JPanel countryFlagPanel;
@@ -37,14 +41,42 @@ public class CountryInfoUtilityUI {
 	JLabel countryLanguageResultLabel;
 	
 	public CountryInfoUtilityUI() {
+		// Set the main application frame
 		mainUIFrame = new JFrame("SOAP Retrieval Utility");
 		mainUIFrame.setSize(800, 560);
 		mainUIFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		mainUIFrame.setResizable(false);
 		
+		// The JPanel where all the widgets will be placed
 		componentPanel = new JPanel();
 		componentPanel.setLayout(null);
 		mainUIFrame.setContentPane(componentPanel);
+		
+		// Title Widget for the application
+		titleLabel = new JLabel("Country SOAP Detail Extractor");
+		titleLabel.setFont(new Font("Serif", Font.BOLD, 35));
+		titleLabel.setBounds(30, 30, 740 , 35);
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+		componentPanel.add(titleLabel);
+		
+		// Instruction Label
+		inputLabel = new JLabel("Select Continent and Country: ");
+		inputLabel.setFont(new Font("Serif", Font.PLAIN, 15));
+		inputLabel.setBounds(30, 100, 250, 15);
+		inputLabel.setHorizontalAlignment(JLabel.CENTER);
+		componentPanel.add(inputLabel);
+		
+		// ComboBox for selecting continent
+		continentComboBox = new JComboBox();
+		continentComboBox.setBounds(300, 102, 150, 15);
+		continentComboBox.setEditable(false);
+		componentPanel.add(continentComboBox);
+		
+		// ComboBox for selecting Country
+		countryComboBox = new JComboBox();
+		countryComboBox.setBounds(480, 102, 150, 15);
+		countryComboBox.setEditable(false);
+		componentPanel.add(countryComboBox);
 		
 		mainUIFrame.setVisible(true);		
 	}
