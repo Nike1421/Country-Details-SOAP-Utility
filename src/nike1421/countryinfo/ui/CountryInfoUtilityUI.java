ebd1fd9964cbf6b2e22118614f25acc7bb398773
Nike1421 <ommiemo54@gmail.com>
2023-06-27 21:34:58 +0530

    Declare all the components

---
+++ src/nike1421/countryinfo/ui/CountryInfoUtilityUI.java
@@ -0,0 +1,39 @@
+package nike1421.countryinfo.ui;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class CountryInfoUtilityUI {
+	JFrame mainUIFrame;
+	JLabel inputLabel;
+	JPanel componentPanel;
+	JButton retrieveResultsButton;
+	JComboBox countinentComboBox;
+	JComboBox countryComboBox;
+	
+	JPanel countryFlagPanel;
+	JLabel countryNameLabel;
+	
+	JLabel countryCapitalLabel;
+	JLabel countryCapitalResultLabel;
+	
+	JLabel countryContinentLabel;
+	JLabel countryContinentResultLabel;
+	
+	JLabel countryPhoneCodeLabel;
+	JLabel countryPhoneCodeResultLabel;
+	
+	JLabel countryISOCodeLabel;
+	JLabel countryISOCodeResultLabel;
+	
+	JLabel countryCurrencyLabel;
+	JLabel countryCurrencyResultLabel;
+	
+	JLabel countryLanguageLabel;
+	JLabel countryLanguageResultLabel;
+	
+}
